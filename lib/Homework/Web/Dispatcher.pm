e672bc12cdc42309be4182cd42f7dd4dbd6ece54
MIZUNE Pine <pinemz@gmail.com>
2014-10-12 09:28:43 +0900

    Update

--- lib/Homework/Web/Dispatcher.pm
+++ lib/Homework/Web/Dispatcher.pm
@@ -4,26 +4,60 @@ use warnings;
 use utf8;
 use Amon2::Web::Dispatcher::RouterBoom;
 
+# 判定処理
+sub is_time_include {
+    my ($comp, $begin, $end) = @_;
+    
+    # 日を跨いでいる場合、区間を分割
+    if ($begin > $end) {
+        return (&is_time_include($comp, $begin, 24) or
+                &is_time_include($comp, 0,      $end));
+    }
+    
+    # 開始時間と同じ場合
+    if ($begin == $comp) { return 1; }
+    
+    # 開始時間より後で、終了時間より前にある場合
+    if ($begin < $comp and $end > $comp) { return 1; }
+    
+    # その他の場合
+    return 0;
+}
+
 any '/' => sub {
     my ($c) = @_;
-    my $counter = $c->session->get('counter') || 0;
-    $counter++;
-    $c->session->set('counter' => $counter);
-    return $c->render('index.tx', {
-        counter => $counter,
-    });
-};
-
-post '/reset_counter' => sub {
-    my $c = shift;
-    $c->session->remove('counter');
-    return $c->redirect('/');
-};
-
-post '/account/logout' => sub {
-    my ($c) = @_;
-    $c->session->expire();
-    return $c->redirect('/');
+    
+    # パラメーターを受け取り
+    my $begin = $c->req->param('begin');
+    my $end   = $c->req->param('end');
+    my $comp  = $c->req->param('comp');
+    
+    # テンプレートパラメーター (初期値)
+    my %tmpl_args = (
+        begin => '',
+        end   => '',
+        comp  => ''
+    );
+    
+    # パラメーターのフォーマットが正しければ、演算処理
+    if (defined($begin) and defined($end) and defined($comp)) {
+        if ($begin !~ /\D/ and $end !~ /\D/ and $comp !~ /\D/) {
+            # テンプレートパラメーターに保存
+            $tmpl_args{"begin"} = $begin;
+            $tmpl_args{"end"}   = $end;
+            $tmpl_args{"comp"}  = $comp;
+            
+            # 整数に変換
+            $begin = int($begin);
+            $end   = int($end);
+            $comp  = int($comp);
+            
+            # 判定処理
+            $tmpl_args{"included"} = &is_time_include($comp, $begin, $end);
+        }
+    }
+    
+    return $c->render('index.tx', \%tmpl_args);
 };
 
 1;
